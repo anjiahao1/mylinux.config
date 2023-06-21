ebd04f1eb395b62d77a79bc54d36553fa08be716
anjiahao <anjiahao@xiaomi.com>
2023-06-21 18:22:13 +0800

    change clangd
    Signed-off-by: anjiahao <anjiahao@xiaomi.com>

--- lvim/config.lua
+++ lvim/config.lua
@@ -47,31 +47,31 @@ lvim.builtin.which_key.mappings["lg"] = {
   { "<cmd>GitBlameToggle<cr>", "Gitblame" },
 }
 
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "clangd" })
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "clangd" })
 
-local clangd_flags = {
-  "--fallback-style=google",
-  "--background-index",
-  "-j=12",
-  "--all-scopes-completion",
-  "--pch-storage=disk",
-  "--clang-tidy",
-  "--log=error",
-  "--completion-style=detailed",
-  "--header-insertion=iwyu",
-  "--header-insertion-decorators",
-  "--enable-config",
-  "--offset-encoding=utf-16",
-  "--ranking-model=heuristics",
-  "--folding-ranges",
-}
+-- local clangd_flags = {
+--   "--fallback-style=google",
+--   "--background-index",
+--   "-j=12",
+--   "--all-scopes-completion",
+--   "--pch-storage=disk",
+--   "--clang-tidy",
+--   "--log=error",
+--   "--completion-style=detailed",
+--   "--header-insertion=iwyu",
+--   "--header-insertion-decorators",
+--   "--enable-config",
+--   "--offset-encoding=utf-16",
+--   "--ranking-model=heuristics",
+--   "--folding-ranges",
+-- }
 
-local clangd_bin = "clangd"
+-- local clangd_bin = "clangd"
 
-local opts = {
-  cmd = { clangd_bin, unpack(clangd_flags) },
-}
-require("lvim.lsp.manager").setup("clangd", opts)
+-- local opts = {
+--   cmd = { clangd_bin, unpack(clangd_flags) },
+-- }
+-- require("lvim.lsp.manager").setup("clangd", opts)
 
 -- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 -- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
